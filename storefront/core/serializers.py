a866d3dfd7afb56e5953aac8fb186e664aa306c7
Shaquaile <shaquaile.w@gmail.com>
2023-12-28 23:48:14 -0500

    changed core views

--- storefront/core/serializers.py
+++ storefront/core/serializers.py
@@ -1,5 +1,7 @@
 from store.models import Customer
 from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
@@ -11,3 +13,23 @@ class UserCreateSerializer(BaseUserCreateSerializer):
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        
+        
+
+User = get_user_model()
+
+class CustomUserCreationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            password=validated_data['password']
+        )
+        return user
--- storefront/core/views.py
+++ storefront/core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from djoser.views import *
 from rest_framework import generics
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin
 from core.serializers import UserCreateSerializer, UserSerializer
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth import get_user_model
+from .serializers import CustomUserCreationSerializer
 
 # from django.views.generic import TemplateView
 
@@ -21,17 +23,32 @@ from .forms import CustomUserCreationForm
 #     def get_object(self):
 #         return self.request.user  # Retrieve the currently authenticated user
 
-def signUpView(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # After saving, you can redirect the user or perform other actions
-            return redirect('/')
-    else:
-        form = CustomUserCreationForm()
+# class SignUpView(ModelViewSet):
+#     def 
+#         if request.method == 'POST':
+#             form = CustomUserCreationForm(request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 # After saving, you can redirect the user or perform other actions
+#                 print(form)
+#                 return redirect('/')
+#         else:
+#             form = CustomUserCreationForm()
 
-    return render(request, 'core/signUp.html', {'form': form})
+#         return render(request, 'core/signUp.html', {'form': form})
+
+
+
+
+User = get_user_model()
+
+class SignUpViewSet(GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = CustomUserCreationSerializer
+    def get(self):
+        if self.request.method == 'GET':
+            
+            print(serializer.data)
 
 
 def signInView(request):
